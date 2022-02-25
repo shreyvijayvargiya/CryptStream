fbddb0ebe4f7016c0d5a1b38b14958d4b6604994
shreykoo <shrey.vijayvargiya@kooapp.com>
2022-02-26 00:27:00 +0530

    title changed and padding and margin removed from body

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,7 @@ const Home = () => {
                 <meta name="keywords" content="React, Node JS,Frontend, Backend, Blockhain" />
                 <meta name="author" content="shrey vijayvargiya" />
 
-                <meta name="twitter:title" content="iHateReading" />
+                <meta name="twitter:title" content="CryptStream" />
                 <meta name="twitter:description" content="All crytpo streams at one place from NFT to tokens"  />
                 <meta property="twitter:image" content="https://firebasestorage.googleapis.com/v0/b/ihatereading-4ba52.appspot.com/o/Company%2Fbanner.png?alt=media&token=d027a34e-dc55-45b4-be52-6fc1503cb511" />
                 <meta name="twitter:card" content="summary_large_image" />
