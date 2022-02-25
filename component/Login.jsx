1527be61011a0de08fe21a0d66700f6deaddabd4
shreykoo <shrey.vijayvargiya@kooapp.com>
2022-02-25 13:40:43 +0530

    login page created

--- component/Login.jsx
+++ component/Login.jsx
@@ -21,32 +21,33 @@ const Login = () => {
                 router.push("/dashboard?type=transfers");
             }, onError: (error) =>{
                 console.log("Error in connecting wallet", error);
-                console.log(authError, 'authError')
+                console.log(authError, 'authError');
+                alert("Error in connecting metamask")
                 setDisabled(false);
             }});
         }else {
-            router.push("/dashboard");
+            router.push("/dashboard?type=transfers");
         }
 	};
 
 
     useEffect(() => {
         if(isAuthenticated && user){
-            router.push("/dashboard");
+            router.push("/dashboard?type=transfers");
         }
     }, []);
 
 	return (
-		<div className="flex justify-center items-center flex-col h-screen" style={{ background: 'linear-gradient(90deg, rgba(106, 133, 182, 0.5) 0%, rgba(186, 200, 224, 0.5) 100%)' }}>
+		<div className="flex justify-center items-center relative flex-col h-screen" style={{ background: 'linear-gradient(90deg, rgba(106, 133, 182, 0.5) 0%, rgba(186, 200, 224, 0.5) 100%)' }}>
             <div className="text-center p-4">
                 <h2 className="text-2xl font-bold">CrytpStream</h2>
-                <p> All crytpo streams at once place</p>
-                <p className="text-xs">Cross Compatible accross all blockhain networks</p>
+                <p className="my-2"> All crytpo streams at once place</p>
+                <p className="text-xs">Cross Compatible accross blockhain networks</p>
                 <br />
                 <Button onClick={login} size="small" color="primary" variant="contained" disabled={disabled}
-                    style={{ backgroundColor: 'black', color: 'white' }}
+                    style={{ backgroundColor: 'black', color: 'white', minWidth: '300px' }}
                 >
-                    Connect <img src="/metamaskLogo.svg" className="mx-2"  /> Wallet
+                    Connect <img src="/metamaskLogo.svg" className="mx-2 my-2"  /> Wallet
                 </Button>
             </div>
 		</div>
