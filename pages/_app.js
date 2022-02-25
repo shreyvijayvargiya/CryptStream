0c86bdeac199f2362a7bb146e5faa881c0b00f92
shreykoo <shrey.vijayvargiya@kooapp.com>
2022-02-25 23:50:39 +0530

    meta tags removed

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ const MyApp = ({ Component, pageProps }) => {
 
     return (
         <MoralisProvider appId={process.env.appId} serverUrl={process.env.serverUrl}>
-            <Head>
+            {/* <Head>
                 <title>CryptStream</title>
                 <meta name="title" content="CrytpStream" />
                 <meta name="description" content="All crytpo streams at one place from NFT to tokens" />
@@ -22,7 +22,7 @@ const MyApp = ({ Component, pageProps }) => {
                 <meta property="twitter:image" content="https://firebasestorage.googleapis.com/v0/b/ihatereading-4ba52.appspot.com/o/Company%2Fbanner.png?alt=media&token=d027a34e-dc55-45b4-be52-6fc1503cb511" />
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta name="twitter:site" content="CryptStream | All crytpo streams at one place from NFT to tokens" />
-            </Head>
+            </Head> */}
             <Component {...pageProps} />
         </MoralisProvider>
     );
